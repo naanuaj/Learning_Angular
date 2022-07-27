ca0d07d43c600ffb7114f6aaec2e3f8680c17bb9
Ajay <ajayaju23@outlook.com>
2022-07-27 13:15:34 +0530

    BasicApp Without any inter component communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: RecipeModel[] = [
+    new RecipeModel('A Test Recipe', 'This is simply a test', 
+      'https://www.vegrecipesofindia.com/wp-content/uploads/2018/06/murukku-recipe-1a.jpg'),
+      new RecipeModel('A Test Recipe', 'This is simply a test', 
+      'https://www.vegrecipesofindia.com/wp-content/uploads/2018/06/murukku-recipe-1a.jpg')
+  ]
 
   constructor() { }
 
